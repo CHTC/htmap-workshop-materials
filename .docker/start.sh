a25aaa6f9033437f0ccee999dfa464ec3883a9dc
JoshKarpel <karpel@wisc.edu>
2019-09-06 10:35:11 -0500

    no need for docker

--- .docker/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-CONTAINER_TAG=htmap-workshop
-
-docker build -t ${CONTAINER_TAG} --file .docker/Dockerfile .
-docker run -it --rm --mount type=bind,source="$(pwd)",target=/home/me/workshop ${CONTAINER_TAG} bash
